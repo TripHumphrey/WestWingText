af0ce8033beba46668314af1fdc5c467cb460c33
Trip Humphrey <tehumphr@ads.bshsi.com>
2017-05-15 07:41:07 -0400

    got rid of old testing code

--- main.R
+++ main.R
@@ -1,5 +1,19 @@
 library(rvest)
 
-a <- read_html("http://www.westwingtranscripts.com/search.php?flag=getTranscript&id=1") %>%
+# Using a for loop (for now) to cycle through the west wing transcripts and load them 
+df <- data.frame("ep_no" = 1:148) %>%
+  mutate(episode_url =paste0("http://www.westwingtranscripts.com/search.php?flag=getTranscript&id=",ep_no))
+
+for (i in df$episode_url) {
+df$episode_txt[i] <- read_html(df$episode_url[i]) %>%
   html_nodes("pre") %>%
   html_text()
+} 
+
+test <- df$episode_url %>% map(read_html)
+
+maybe <- list()
+for (i in 1:148) {
+maybe[i] <- html_nodes(test[[i]], "pre") %>%
+    html_text()
+}
